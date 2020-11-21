660af99ee576435c0315722d3a4379e3f020170d
Filire09 <felixrobillard@gmail.com>
2020-11-21 18:20:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+const Discord = require("discord.js");
+const prefix = "!";
+Client = new Discord.Client;
+
+Client.on("ready", () => {
+    console.log("The bot is ready to be used");
+
+})
+
+Client.login("Nzc5ODIyNjUxMzIwMzAzNjY3.X7mIcA.NrnoLU8hZA2bkB_zVpTmraKLQ5o")
